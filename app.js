3e44770dcfb253c2686d9a9b2da14932cec37d33
Amar1013 <amarjithraj67@gmail.com>
2022-09-06 18:32:40 +0530

    editing field temporarily fine

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ let btn2 = document.querySelector(".edit");
 todoText.addEventListener("keypress",(event)=>{
   if (event.key == "Enter"){
     // Cancel the default action, if needed
-    // event.preventDefault();
+    event.preventDefault();
     // console.log(event.key)
     // to triger the click event of the add todo button
     if (todoText.value!="") {
@@ -36,13 +36,13 @@ btn.addEventListener("click", () => {
 
   if (todoText.value!="") {
     let span = document.createElement("span");
-    span.innerText = todoText.value;
-    console.log(span.innerText)
-    li.style.cssText = "animation-name:slide";
     items.appendChild(li);
     li.appendChild(span);
     li.appendChild(trash);
     li.appendChild(edit);
+    span.innerText = todoText.value;
+    console.log(span.innerText)
+    li.style.cssText = "animation-name:slide";
     todoText.focus();
     todoText.value = " ";
   }
@@ -67,38 +67,42 @@ items.addEventListener("click", (event) => {
 });
 
 //edit an element
-items.addEventListener("click", (event) => {
+items.addEventListener("click", event => {
   if (event.target.classList[1] === "fa-edit") {
-    console.log(event.target.classList[1]);
-    btn2.style.display = "block";
-    btn.style.display = "none";
-    let element = event.target.parentElement;
-    // console.log(element)
-    todoText.value = element.innerText;
-    console.log("todo:",todoText.value);
-    todoText.style.color = "#00ffff";
-    editLogic(element);   
-  }
+    // console.log(event.target.classList[1]);
+  //   btn2.style.display = "block";
+  //   btn.style.display = "none";
+  //   let element = event.target.parentElement;
+  //   let _span = element.querySelector('span')
+  //   console.log("todo:",_span);
+  //   console.log(element)
+  //   todoText.value = element.innerText;
+  //   todoText.style.color = "#00ffff";
+  //   btn2.addEventListener("click", () => {
+  //     if (todoText.value!=""  ){
+  //     _span.innerText = todoText.value;
+  //     console.log(_span,_span.innerText)
+  //     let p = document.createElement("p");
+  //     p.classList.add("msg");
+  //     p.innerText = "edited todo successfully!";
+  //     // p.style.cssText="color:lightgreen; font-size:10px;"
+  //     msg.insertBefore(p, msg.firstChild);
+  //     p.addEventListener("transitionend", () => {
+  //       p.remove();
+  //   });
+  //   }
+  //   btn2.style.display = "none";
+  //   btn.style.display = "block";
+  //   todoText.value = "";
+  //   });
+  // }
+  let val = prompt("enter new value")
+  let element = event.target.parentElement;
+  let span = element.querySelector("span");
+  span.innerText=val;}
 });
 
-function editLogic(element){
-    let span = element.querySelector('span')
-    btn2.addEventListener("click", () => {
-        span.innerHTML = todoText.value;
-        console.log(span.innerText)
-        todoText.value = "";
-        let p = document.createElement("p");
-        p.classList.add("msg");
-        p.innerText = "edited todo successfully!";
-        // p.style.cssText="color:lightgreen; font-size:10px;"
-        msg.insertBefore(p, msg.firstChild);
-        p.addEventListener("transitionend", () => {
-          p.remove();
-      });
-      btn2.style.display = "none";
-      btn.style.display = "block";
-      });
-}
+
 
 //check or unchecking an element
 items.addEventListener("click", (event) => {
